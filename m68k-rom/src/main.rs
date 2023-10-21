73df55b74f65f1cf3f1a11472aae6e10c728a24e
Anthony Guerrero <anthony@anthony.moe>
2023-10-21 13:07:18 -0700

    Add trap instructions

--- m68k-rom/src/main.rs
+++ m68k-rom/src/main.rs
@@ -2,9 +2,34 @@
 #![no_std]
 
 use m68k_rt::entry;
+use m68k::asm::{Trap, trap};
 extern crate panic_abort;
+
+struct RegisterBlock {
+    pub reg: u8,
+}
+
+struct Device {
+    pub p: &'static mut RegisterBlock,
+}
+
+impl Device {
+    fn new() -> Device {
+        Device {
+            p: unsafe { &mut *(0x0080_0000 as *mut RegisterBlock) }
+        }
+    }
+}
+
+pub fn putc() {
+    let mut uart = Device::new();
+    
+    uart.p.reg = 5;
+}
 
 #[entry]
 unsafe fn main() -> ! {
+    trap(Trap::Trap0);
+    putc();
     loop {}
 }
--- m68k/src/asm.rs
+++ m68k/src/asm.rs
@@ -19,3 +19,45 @@ pub fn illegal() -> ! {
         asm!("illegal", options(noreturn, nomem, nostack, preserves_flags))
     }
 }
+pub enum Trap {
+    Trap0,
+    Trap1,
+    Trap2,
+    Trap3,
+    Trap4,
+    Trap5,
+    Trap6,
+    Trap7,
+    Trap8,
+    Trap9,
+    Trap10,
+    Trap11,
+    Trap12,
+    Trap13,
+    Trap14,
+    Trap15,
+}
+
+#[inline]
+pub fn trap(t: Trap) {
+    unsafe {
+        match t {
+            Trap::Trap0 => asm!("trap #0"),
+            Trap::Trap1 => asm!("trap #1"),
+            Trap::Trap2 => asm!("trap #2"),
+            Trap::Trap3 => asm!("trap #3"),
+            Trap::Trap4 => asm!("trap #4"),
+            Trap::Trap5 => asm!("trap #5"),
+            Trap::Trap6 => asm!("trap #6"),
+            Trap::Trap7 => asm!("trap #7"),
+            Trap::Trap8 => asm!("trap #8"),
+            Trap::Trap9 => asm!("trap #9"),
+            Trap::Trap10 => asm!("trap #10"),
+            Trap::Trap11 => asm!("trap #11"),
+            Trap::Trap12 => asm!("trap #12"),
+            Trap::Trap13 => asm!("trap #13"),
+            Trap::Trap14 => asm!("trap #14"),
+            Trap::Trap15 => asm!("trap #15"),
+        }
+    }
+}
